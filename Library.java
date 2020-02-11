ed9a0d7c0180b1e3ec02a8592e069a6cea67e13d
Ankit252697 <60807533+Ankit252697@users.noreply.github.com>
2020-02-11 04:14:26 -0800

    Update and rename Liberary.java to Library.java

--- Library.java
+++ Library.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-public class Liberary {
+public class Library {
 	public static void main(String []args) throws Exception
 	{
 		List<AddBook> ablist=new ArrayList<AddBook>();
